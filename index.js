be5c282237c32090b4c16f1a3fd05eae7901e578
Onakoya Kayode <onakoyakayode@gmail.com>
2022-02-05 15:38:58 +0100

    index js

---
+++ index.js
@@ -0,0 +1,202 @@
+
+
+
+
+
+function h1() {
+    let food = document.getElementById('h1');
+    let foodName = document.getElementById('h11');
+    let foodDesc = document.getElementById('h12');
+    let button71 = document.getElementById('button71');
+
+    food.addEventListener('mouseover', () => {
+        foodName.style.transform = 'translateY(-40px)';
+        foodDesc.style.opacity = '1';
+        foodDesc.style.transform = 'translateY(-40px)';
+        foodName.style.transition = 'all 0.6s ease-in-out';
+        food.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0, 0.8)),url(icecream.jpg)';
+        button71.style.transform = 'translateY(-30px)';
+        button71.style.transition = 'all 0.8s ease-in-out';
+        button71.style.opacity = '1';
+
+
+
+        food.addEventListener('mouseleave', () => {
+            foodName.style.transform = 'translateY(40px)'
+            foodDesc.style.opacity = '0'
+            foodDesc.style.transform = 'translateY(40px)'
+            foodName.style.transition = 'all 0.6s ease-in-out'
+            food.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0, 0.4)),url(icecream.jpg)'
+            button71.style.transform = 'translateY(30px)'
+            button71.style.transition = 'all 0.8s ease-in-out'
+            button71.style.opacity = '0'
+        })
+    })
+
+    let food2 = document.getElementById('h2');
+    let foodName2 = document.getElementById('h21');
+    let foodDesc2 = document.getElementById('h22');
+    let button72 = document.getElementById('button72')
+
+    food2.addEventListener('mouseover', () => {
+        foodName2.style.transform = 'translateY(-40px)'
+        foodDesc2.style.opacity = '1'
+        foodDesc2.style.transform = 'translateY(-40px)'
+        foodName2.style.transition = 'all 0.6s ease-in-out'
+        food2.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(snacks.jpg)'
+        button72.style.transform = 'translateY(-30px)'
+        button72.style.transition = 'all 0.8s ease-in-out'
+        button72.style.opacity = '1'
+
+
+        food2.addEventListener('mouseleave', () => {
+            foodName2.style.transform = 'translateY(40px)'
+            foodDesc2.style.opacity = '0'
+            foodDesc2.style.transform = 'translateY(40px)'
+            foodName2.style.transition = 'all 0.6s ease-in-out'
+            food2.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(snacks.jpg)'
+            button72.style.transform = 'translateY(30px)'
+            button72.style.transition = 'all 0.8s ease-in-out'
+            button72.style.opacity = '0'
+        })
+    })
+
+    let food3 = document.getElementById('h3');
+    let foodName3 = document.getElementById('h31');
+    let foodDesc3 = document.getElementById('h32');
+    let button73 = document.getElementById('button73')
+
+    food3.addEventListener('mouseover', () => {
+        foodName3.style.transform = 'translateY(-40px)'
+        foodDesc3.style.opacity = '1'
+        foodDesc3.style.transform = 'translateY(-40px)'
+        foodName3.style.transition = 'all 0.6s ease-in-out'
+        food3.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(cake1.jpg)'
+        button73.style.transform = 'translateY(20px)'
+        button73.style.transition = 'all 0.8s ease-in-out'
+        button73.style.opacity = '1'
+
+
+
+        food3.addEventListener('mouseleave', () => {
+            foodName3.style.transform = 'translateY(40px)'
+            foodDesc3.style.opacity = '0'
+            foodDesc3.style.transform = 'translateY(40px)'
+            foodName3.style.transition = 'all 0.6s ease-in-out'
+            food3.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(cake4.jpg)'
+            button73.style.transform = 'translateY(-20px)'
+            button73.style.transition = 'all 0.8s ease-in-out'
+            button73.style.opacity = '0'
+        })
+    })
+
+    let food4 = document.getElementById('h4');
+    let foodName4 = document.getElementById('h41');
+    let foodDesc4 = document.getElementById('h42');
+    let button74 = document.getElementById('button74');
+
+    food4.addEventListener('mouseover', () => {
+        foodName4.style.transform = 'translateY(-40px)'
+        foodDesc4.style.opacity = '1'
+        foodDesc4.style.transform = 'translateY(-40px)'
+        foodName4.style.transition = 'all 0.6s ease-in-out'
+        food4.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(chineserice.jpg)'
+        button74.style.transform = 'translateY(-10px)'
+        button74.style.transition = 'all 0.8s ease-in-out'
+        button74.style.opacity = '1'
+
+
+
+        food4.addEventListener('mouseleave', () => {
+            foodName4.style.transform = 'translateY(40px)'
+            foodDesc4.style.opacity = '0'
+            foodDesc4.style.transform = 'translateY(40px)'
+            foodName4.style.transition = 'all 0.6s ease-in-out'
+            food4.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(chineserice.jpg)'
+            button74.style.transform = 'translateY(30px)'
+            button74.style.transition = 'all 0.8s ease-in-out'
+            button74.style.opacity = '0'
+        })
+    })
+
+    let food5 = document.getElementById('h5');
+    let foodName5 = document.getElementById('h51');
+    let foodDesc5 = document.getElementById('h52');
+    let button75 = document.getElementById('button75');
+
+    food5.addEventListener('mouseover', () => {
+        foodName5.style.transform = 'translateY(-40px)'
+        foodDesc5.style.opacity = '1'
+        foodDesc5.style.transform = 'translateY(-40px)'
+        foodName5.style.transition = 'all 0.6s ease-in-out'
+        food5.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(chinese-rice.jpg)'
+        button75.style.transform = 'translateY(-10px)'
+        button75.style.transition = 'all 0.8s ease-in-out'
+        button75.style.opacity = '1'
+
+
+
+        food5.addEventListener('mouseleave', () => {
+            foodName5.style.transform = 'translateY(40px)'
+            foodDesc5.style.opacity = '0'
+            foodDesc5.style.transform = 'translateY(40px)'
+            foodName5.style.transition = 'all 0.6s ease-in-out'
+            food5.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(chinese-rice.jpg)'
+            button75.style.transform = 'translateY(30px)'
+            button75.style.transition = 'all 0.8s ease-in-out'
+            button75.style.opacity = '0'
+        })
+    })
+
+    let food6 = document.getElementById('h6');
+    let foodName6 = document.getElementById('h61');
+    let foodDesc6 = document.getElementById('h62');
+    let button76 = document.getElementById('button76');
+
+    food6.addEventListener('mouseover', () => {
+        foodName6.style.transform = 'translateY(-40px)'
+        foodDesc6.style.opacity = '1'
+        foodDesc6.style.transform = 'translateY(-40px)'
+        foodName6.style.transition = 'all 0.6s ease-in-out'
+        food6.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(bbq1.jpg)'
+        button76.style.transform = 'translateY(0px)'
+        button76.style.transition = 'all 0.8s ease-in-out'
+        button76.style.opacity = '1'
+
+
+
+        food6.addEventListener('mouseleave', () => {
+            foodName6.style.transform = 'translateY(40px)'
+            foodDesc6.style.opacity = '0'
+            foodDesc6.style.transform = 'translateY(40px)'
+            foodName6.style.transition = 'all 0.6s ease-in-out'
+            food6.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(bbq.jpg)'
+            button76.style.transform = 'translateY(40px)'
+            button76.style.transition = 'all 0.8s ease-in-out'
+            button76.style.opacity = '0'
+        })
+    })
+
+}
+
+h1();
+
+
+function about() {
+    location.href = "About-Us.html"
+}
+
+function hamburgMenu() {
+    let menu = document.getElementById('menu1');
+    let navBar = document.getElementById('navbar');
+    let closeBar = document.getElementById('closeBar')
+
+    menu.addEventListener('click', () => {
+        navBar.style.left = '100%';
+        navBar.style.left = '0'
+    })
+
+    closeBar.addEventListener('click', () => {
+        navBar.style.left = '-100%'
+    })
+};
