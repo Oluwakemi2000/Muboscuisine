c4a00bb559c3d5c9cd0ffecca2247b0075632e05
Onakoya Kayode <onakoyakayode@gmail.com>
2022-02-05 15:35:15 +0100

    app js file

--- app.js
+++ app.js
@@ -76,16 +76,16 @@ function kay() {
 
     grid4.addEventListener('mouseover', () => {
         cook3.style.color = 'white'
-        cook3.style.transform = 'translateY(-60px)'
-        cook3.style.transition = 'all 0.5s ease-in-out'
+        cook3.style.transform = 'translateY(-40px)'
+        cook3.style.transition = 'all 0.6s ease-in-out'
         grid4.style.background = 'linear-gradient(to top right, #00534e, #0e4732)'
         para3.style.opacity = '1'
         para3.style.transition = 'all 0.6s ease-in-out'
-        para3.style.transform = 'translateY(-60px)'
+        para3.style.transform = 'translateY(-40px)'
 
 
         grid4.addEventListener('mouseleave', () => {
-            cook3.style.transform = 'translateY(0px)'
+            cook3.style.transform = 'translateY(40px)'
             cook3.style.transition = 'all 0.6s ease-in-out'
             grid4.style.background = 'linear-gradient(to top right, #00dacd, #31f4ad)'
             para3.style.opacity = '0'
@@ -147,13 +147,17 @@ function h1() {
     let food = document.getElementById('h1');
     let foodName = document.getElementById('h11');
     let foodDesc = document.getElementById('h12');
+    let button71 = document.getElementById('button71');
 
     food.addEventListener('mouseover', () => {
-        foodName.style.transform = 'translateY(-40px)'
-        foodDesc.style.opacity = '1'
-        foodDesc.style.transform = 'translateY(-40px)'
-        foodName.style.transition = 'all 0.6s ease-in-out'
-        food.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0, 0.8)),url(icecream.jpg)'
+        foodName.style.transform = 'translateY(-40px)';
+        foodDesc.style.opacity = '1';
+        foodDesc.style.transform = 'translateY(-40px)';
+        foodName.style.transition = 'all 0.6s ease-in-out';
+        food.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0, 0.8)),url(icecream.jpg)';
+        button71.style.transform = 'translateY(-30px)';
+        button71.style.transition = 'all 0.8s ease-in-out';
+        button71.style.opacity = '1';
 
 
 
@@ -163,12 +167,16 @@ function h1() {
             foodDesc.style.transform = 'translateY(40px)'
             foodName.style.transition = 'all 0.6s ease-in-out'
             food.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0, 0.4)),url(icecream.jpg)'
+            button71.style.transform = 'translateY(30px)'
+            button71.style.transition = 'all 0.8s ease-in-out'
+            button71.style.opacity = '0'
         })
     })
 
     let food2 = document.getElementById('h2');
     let foodName2 = document.getElementById('h21');
     let foodDesc2 = document.getElementById('h22');
+    let button72 = document.getElementById('button72')
 
     food2.addEventListener('mouseover', () => {
         foodName2.style.transform = 'translateY(-40px)'
@@ -176,7 +184,9 @@ function h1() {
         foodDesc2.style.transform = 'translateY(-40px)'
         foodName2.style.transition = 'all 0.6s ease-in-out'
         food2.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(snacks.jpg)'
-
+        button72.style.transform = 'translateY(-30px)'
+        button72.style.transition = 'all 0.8s ease-in-out'
+        button72.style.opacity = '1'
 
 
         food2.addEventListener('mouseleave', () => {
@@ -185,12 +195,16 @@ function h1() {
             foodDesc2.style.transform = 'translateY(40px)'
             foodName2.style.transition = 'all 0.6s ease-in-out'
             food2.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(snacks.jpg)'
+            button72.style.transform = 'translateY(30px)'
+            button72.style.transition = 'all 0.8s ease-in-out'
+            button72.style.opacity = '0'
         })
     })
 
     let food3 = document.getElementById('h3');
     let foodName3 = document.getElementById('h31');
     let foodDesc3 = document.getElementById('h32');
+    let button73 = document.getElementById('button73')
 
     food3.addEventListener('mouseover', () => {
         foodName3.style.transform = 'translateY(-40px)'
@@ -198,6 +212,9 @@ function h1() {
         foodDesc3.style.transform = 'translateY(-40px)'
         foodName3.style.transition = 'all 0.6s ease-in-out'
         food3.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(cake1.jpg)'
+        button73.style.transform = 'translateY(20px)'
+        button73.style.transition = 'all 0.8s ease-in-out'
+        button73.style.opacity = '1'
 
 
 
@@ -207,12 +224,16 @@ function h1() {
             foodDesc3.style.transform = 'translateY(40px)'
             foodName3.style.transition = 'all 0.6s ease-in-out'
             food3.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(cake4.jpg)'
+            button73.style.transform = 'translateY(-20px)'
+            button73.style.transition = 'all 0.8s ease-in-out'
+            button73.style.opacity = '0'
         })
     })
 
     let food4 = document.getElementById('h4');
     let foodName4 = document.getElementById('h41');
     let foodDesc4 = document.getElementById('h42');
+    let button74 = document.getElementById('button74');
 
     food4.addEventListener('mouseover', () => {
         foodName4.style.transform = 'translateY(-40px)'
@@ -220,6 +241,9 @@ function h1() {
         foodDesc4.style.transform = 'translateY(-40px)'
         foodName4.style.transition = 'all 0.6s ease-in-out'
         food4.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(chineserice.jpg)'
+        button74.style.transform = 'translateY(-10px)'
+        button74.style.transition = 'all 0.8s ease-in-out'
+        button74.style.opacity = '1'
 
 
 
@@ -229,12 +253,16 @@ function h1() {
             foodDesc4.style.transform = 'translateY(40px)'
             foodName4.style.transition = 'all 0.6s ease-in-out'
             food4.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(chineserice.jpg)'
+            button74.style.transform = 'translateY(30px)'
+            button74.style.transition = 'all 0.8s ease-in-out'
+            button74.style.opacity = '0'
         })
     })
 
     let food5 = document.getElementById('h5');
     let foodName5 = document.getElementById('h51');
     let foodDesc5 = document.getElementById('h52');
+    let button75 = document.getElementById('button75');
 
     food5.addEventListener('mouseover', () => {
         foodName5.style.transform = 'translateY(-40px)'
@@ -242,6 +270,9 @@ function h1() {
         foodDesc5.style.transform = 'translateY(-40px)'
         foodName5.style.transition = 'all 0.6s ease-in-out'
         food5.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(chinese-rice.jpg)'
+        button75.style.transform = 'translateY(-10px)'
+        button75.style.transition = 'all 0.8s ease-in-out'
+        button75.style.opacity = '1'
 
 
 
@@ -251,12 +282,16 @@ function h1() {
             foodDesc5.style.transform = 'translateY(40px)'
             foodName5.style.transition = 'all 0.6s ease-in-out'
             food5.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(chinese-rice.jpg)'
+            button75.style.transform = 'translateY(30px)'
+            button75.style.transition = 'all 0.8s ease-in-out'
+            button75.style.opacity = '0'
         })
     })
 
     let food6 = document.getElementById('h6');
     let foodName6 = document.getElementById('h61');
     let foodDesc6 = document.getElementById('h62');
+    let button76 = document.getElementById('button76');
 
     food6.addEventListener('mouseover', () => {
         foodName6.style.transform = 'translateY(-40px)'
@@ -264,6 +299,9 @@ function h1() {
         foodDesc6.style.transform = 'translateY(-40px)'
         foodName6.style.transition = 'all 0.6s ease-in-out'
         food6.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.8)), url(bbq1.jpg)'
+        button76.style.transform = 'translateY(0px)'
+        button76.style.transition = 'all 0.8s ease-in-out'
+        button76.style.opacity = '1'
 
 
 
@@ -273,6 +311,9 @@ function h1() {
             foodDesc6.style.transform = 'translateY(40px)'
             foodName6.style.transition = 'all 0.6s ease-in-out'
             food6.style.backgroundImage = 'linear-gradient(rgba(0,0,0, 0.2), rgba(0,0,0, 0.4)), url(bbq.jpg)'
+            button76.style.transform = 'translateY(40px)'
+            button76.style.transition = 'all 0.8s ease-in-out'
+            button76.style.opacity = '0'
         })
     })
 
@@ -295,7 +336,26 @@ function hamburgMenu() {
     closeBar.addEventListener('click', () => {
         navBar.style.left = '-100%'
     })
+};
 
-   
+
+function validateForm() {
+    let f = document.forms["form1"].value;
+    if (f == "") {
+        alert('Name and Phone No is required');
+    } else {
+        return false;
+    }
+
+};
+
+function button71() {
+    location.href = "dessert.html";
 }
+
+
+let about = () => {
+    location.href = "About-Us.html"
+}
+
 
